a625b5f6a71045a80f2b1cbd5ec2fccbb76ae0ee
Liam <getliam@icloud.com>
2021-03-03 12:05:57 +0100

    cldi

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,15 @@ module.exports = {
             },
         },
         {
+            resolve: 'gatsby-transformer-cloudinary',
+            options: {
+              cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+              apiKey: process.env.CLOUDINARY_API_KEY,
+              apiSecret: process.env.CLOUDINARY_API_SECRET,
+              uploadFolder: 'ghost-blog-images',
+            },
+          },
+        {
             resolve: `gatsby-plugin-ghost-images`,
             options: {
                 // An array of node types and image fields per node
