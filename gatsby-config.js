dd1319a6d1dc57b237b0a21eed7c5c003872ab20
Liam <getliam@icloud.com>
2021-04-12 16:41:59 +0200

    rm ga up in gta4

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,16 +52,23 @@ module.exports = {
             },
          },
          {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-              // The property ID; the tracking code won't be generated without it
-              trackingId: "G-9VXQC7HC6S",
-              // Defines where to place the tracking script - `true` in the head and `false` in the body
-              head: true,
-              // Defers execution of google analytics script after page load
-              defer: false,
+              // You can add multiple tracking ids and a pageview event will be fired for all of them.
+              trackingIds: [
+                "G-9VXQC7HC6S", // Google Analytics / GA
+              ],
+              // This object gets passed directly to the gtag config command
+              // This config will be shared across all trackingIds
+              gtagConfig: {
+              },
+              // This object is used for configuration specific to this plugin
+              pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: true,
+              },
             },
-        },
+          },
         {
             resolve: `gatsby-source-filesystem`,
             options: {
