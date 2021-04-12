90250f3d090c2c11a0a8ad6f8c58603d36257e68
Liam <getliam@icloud.com>
2021-04-12 15:45:56 +0200

    ga true

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
               // The property ID; the tracking code won't be generated without it
               trackingId: "G-9VXQC7HC6S",
               // Defines where to place the tracking script - `true` in the head and `false` in the body
-              head: false,
+              head: true,
               // Defers execution of google analytics script after page load
               defer: false,
             },
