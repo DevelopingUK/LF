05cbf72d329e87b7523cfbfd853dfe179f2485a4
DevelopingUK <22601590+DevelopingUK@users.noreply.github.com>
2021-04-22 17:58:21 +0200

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,16 +3,23 @@ import { Link } from 'gatsby'
 import { Layout } from '../components/common'
 
 const NotFoundPage = () => (
-    <Layout>
-        <div className="container">
-            <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
-                <section className="content-body">
-                    Page not found, <Link to="/">return home</Link> to start over
-                </section>
-            </article>
+    
+<div class="bg-indigo-900 relative overflow-hidden h-screen">
+    <img src="https://res.cloudinary.com/hhck1gh1f/image/upload/v1619106675/ghost-blog-images/land_mdeve8.svg" class="absolute h-full w-full object-cover"/>
+    <div class="inset-0 bg-black opacity-25 absolute">
+    </div>
+    <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+        <div class="w-full font-mono flex flex-col items-center relative z-10">
+            <h1 class="font-extrabold text-5xl text-center text-white leading-tight mt-4">
+                You&#x27;re alone here
+            </h1>
+            <p class="font-extrabold text-8xl my-44 text-white animate-bounce">
+                404: 
+                <Link to="/">Go Home</Link>
+                </p>
         </div>
-    </Layout>
+    </div>
+</div>
 )
 
 export default NotFoundPage
