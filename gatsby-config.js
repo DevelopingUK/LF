21198ff4dbf44cf47554dc6b275537f223417c06
Liam <getliam@icloud.com>
2021-04-23 22:10:24 +0200

    ed ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,11 +62,13 @@ module.exports = {
               // This object gets passed directly to the gtag config command
               // This config will be shared across all trackingIds
               gtagConfig: {
+                anonymize_ip: true,
+                cookie_expires: 0,
               },
               // This object is used for configuration specific to this plugin
               pluginConfig: {
                 // Puts tracking script in the head instead of the body
-                head: true,
+                head: false,
               },
             },
           },
@@ -113,7 +115,6 @@ module.exports = {
                     {
                         resolve: `gatsby-rehype-inline-images`,
                     },
-                    'gatsby-plugin-no-javascript'
                 ],
             },
         },
