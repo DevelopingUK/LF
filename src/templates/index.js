9493554f7b179ca0a4c017e932a2d6b497845ec8
DevelopingUK <22601590+DevelopingUK@users.noreply.github.com>
2021-06-25 15:36:01 +0200

    home

--- src/templates/index.js
+++ src/templates/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
+
 import { Layout, PostCard, Pagination } from '../components/common'
 import { MetaData } from '../components/common/meta'
 import { useState } from "react";
@@ -31,6 +32,75 @@ const Index = ({ data, location, pageContext }) => {
                 <meta name="description" content="Liam Fallen is a Technical SEO Consultant with a background in web development and marketing." />
             </Helmet>
                 <div className="container">
+
+                <section>
+    <div class="container max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 bg-white dark:bg-gray-800">
+        <div class="flex flex-wrap -mx-8">
+            <div class="w-full lg:w-1/2 px-8">
+                <div class="mb-12 lg:mb-0 pb-12 lg:pb-0 border-b lg:border-b-0">
+                    <h2 class="mb-4 text-3xl lg:text-4xl font-bold font-heading dark:text-white">
+                        If you care about user experience - hire me.
+                    </h2>
+                    <p class="mb-8 leading-loose text-gray-500 dark:text-gray-300">
+                        I'm a Technical SEO that does UX better than most UX professionals. I'm trusted by some of the best in the business including Monday.com, LeoVegas, SurferSEO and ItsNiceThat.
+                    </p>
+                    <div class="w-full md:w-1/3">
+                    <a class="bg-blue-500 rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-6" href="/contact">Hire me</a>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full lg:w-1/2 px-8">
+                <ul class="space-y-12">
+                    <li class="flex -mx-4">
+                        <div class="px-4">
+                            <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full bg-blue-50 text-blue-600">
+                                1
+                            </span>
+                        </div>
+                        <div class="px-4">
+                            <h3 class="my-4 text-xl font-semibold dark:text-white">
+                                Ensure your users are happy!
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-300 leading-loose">
+                            Ensuring your users happy keeps them on your website longer.
+                            </p>
+                        </div>
+                    </li>
+                    <li class="flex -mx-4">
+                        <div class="px-4">
+                            <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full bg-blue-50 text-blue-600">
+                                2
+                            </span>
+                        </div>
+                        <div class="px-4">
+                            <h3 class="my-4 text-xl font-semibold dark:text-white">
+                                Improve performance
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-300 leading-loose">
+                            Faster loading times means your customers can checkout faster.
+                            </p>
+                        </div>
+                    </li>
+                    <li class="flex -mx-4">
+                        <div class="px-4">
+                            <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full bg-blue-50 text-blue-600">
+                                3
+                            </span>
+                        </div>
+                        <div class="px-4">
+                            <h3 class="my-4 text-xl font-semibold dark:text-white">
+                            Increase your conversions.
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-300 leading-loose">
+                            Target high converting keywords that boost your bottom line not your ego.
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
 
                 <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
   <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
@@ -105,84 +175,8 @@ const Index = ({ data, location, pageContext }) => {
     </div>
   </div>
 </div>
-
-                    <section className="post-feed">
-                        {posts.map(({ node }) => (
-                            // The tag below includes the markup for each post - components/common/PostCard.js
-                            <PostCard key={node.id} post={node} />
-                        ))}
-                    </section>
                     <Pagination pageContext={pageContext} />
                 </div>
-
-                <section>
-    <div class="container max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 bg-white dark:bg-gray-800">
-        <div class="flex flex-wrap -mx-8">
-            <div class="w-full lg:w-1/2 px-8">
-                <div class="mb-12 lg:mb-0 pb-12 lg:pb-0 border-b lg:border-b-0">
-                    <h2 class="mb-4 text-3xl lg:text-4xl font-bold font-heading dark:text-white">
-                        If you care about user experience - hire me.
-                    </h2>
-                    <p class="mb-8 leading-loose text-gray-500 dark:text-gray-300">
-                        I'm a Technical SEO that does UX better than most UX professionals. I'm trusted by some of the best in the business including Monday.com, LeoVegas, SurferSEO and ItsNiceThat.
-                    </p>
-                    <div class="w-full md:w-1/3">
-                    <a class="bg-blue-500 rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-6" href="/contact">Hire me</a>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full lg:w-1/2 px-8">
-                <ul class="space-y-12">
-                    <li class="flex -mx-4">
-                        <div class="px-4">
-                            <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full bg-blue-50 text-blue-600">
-                                1
-                            </span>
-                        </div>
-                        <div class="px-4">
-                            <h3 class="my-4 text-xl font-semibold dark:text-white">
-                                Be the best
-                            </h3>
-                            <p class="text-gray-500 dark:text-gray-300 leading-loose">
-                            Be the best at what you do without having to worry about your website.
-                            </p>
-                        </div>
-                    </li>
-                    <li class="flex -mx-4">
-                        <div class="px-4">
-                            <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full bg-blue-50 text-blue-600">
-                                2
-                            </span>
-                        </div>
-                        <div class="px-4">
-                            <h3 class="my-4 text-xl font-semibold dark:text-white">
-                                Take control
-                            </h3>
-                            <p class="text-gray-500 dark:text-gray-300 leading-loose">
-                            Take back control of your website's performance.
-                            </p>
-                        </div>
-                    </li>
-                    <li class="flex -mx-4">
-                        <div class="px-4">
-                            <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full bg-blue-50 text-blue-600">
-                                3
-                            </span>
-                        </div>
-                        <div class="px-4">
-                            <h3 class="my-4 text-xl font-semibold dark:text-white">
-                            Improve
-                            </h3>
-                            <p class="text-gray-500 dark:text-gray-300 leading-loose">
-                            I'll give you the tools you need to create a framework for continuous improvement.
-                            </p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
 
 <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
   <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
